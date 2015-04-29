1aedb9fad7a8ecd4a9254595ac7a0e3bf27342be
Alexander Niema Moshiri <niemamoshiri@gmail.com>
2015-04-28 22:30:09 -0700

    Added Python solution

--- ROSALIND Solutions/Bioinformatics Textbook Track/1H - Frequent Words with Mismatches and Reverse Complements/FrequentWordsWithMismatchesAndReverseComplementsProblem.py
+++ ROSALIND Solutions/Bioinformatics Textbook Track/1H - Frequent Words with Mismatches and Reverse Complements/FrequentWordsWithMismatchesAndReverseComplementsProblem.py
@@ -1,10 +1,11 @@
-def frequentWordsWithMismatches( s, k, d ):
+def frequentWordsWithMismatchesAndReverseComplements( s, k, d ):
     counts = {}
     for i in xrange(len(s)-k+1):
-        for neighbor in neighbors(s[i:i+k],d):
-            if neighbor not in counts:
-                counts[neighbor] = 0
-            counts[neighbor] += 1
+        for sub in [s[i:i+k],reverseComplement(s[i:i+k])]:
+            for neighbor in neighbors(sub,d):
+                if neighbor not in counts:
+                    counts[neighbor] = 0
+                counts[neighbor] += 1
     m = max(counts.values())
     return [kmer for kmer in counts if counts[kmer] == m]
 
@@ -24,7 +25,13 @@ def neighbors( s, d ):
 def hamming( s, t ):
     return sum([s[i] != t[i] for i in xrange(len(s))])
 
+def reverseComplement( s ):
+    return ''.join([complement(s[i]) for i in xrange(len(s)-1,-1,-1)])
+
+def complement( s ):
+    return {'A':'T','T':'A','C':'G','G':'C'}[s]
+
 s = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
 k = 4
 d = 1
-print ' '.join(frequentWordsWithMismatches(s,k,d))
+print ' '.join(frequentWordsWithMismatchesAndReverseComplements(s,k,d))
